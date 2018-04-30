c2a05f9728c73775af14de50f3db15d0dce2855b
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-04-30 12:10:03 +0200

    clean up code

--- whale-categorization-playground/analysis_keras.py
+++ whale-categorization-playground/analysis_keras.py
@@ -11,9 +11,12 @@ from keras.utils import to_categorical
 from tqdm import tqdm
 import numpy as np
 
+NUM_OF_CLASSES = 4251  # len(np.unique(y))# 4251
+
 training_dir = 'data/train/'
 data = pd.read_csv('data/train.csv')
 data['Id_int'] = pd.factorize(data['Id'])[0]
+
 train, test = train_test_split(data, test_size=0.3, shuffle=True, random_state=1337)
 print('Checking training data head')
 print(train.head())
@@ -32,7 +35,7 @@ plt.show()
 
 def process(image):
     # resize
-    image = np.resize(image, [128, 128, 3])
+    image = np.resize(image, [128, 128, 1])
 
     # convert to grayscale
     if image.shape == 3:
@@ -41,33 +44,43 @@ def process(image):
     # return normalized
     return image / 255
 
-x = []
-y = []
 
-
-
-
-for index, path in tqdm(enumerate(train.Image)):
-    """
-    if index>4:
-        break
-    """
-
+def get_image_from_path(path, training_dir, data):
     image = mpimg.imread(training_dir + path)
     image = process(image)
-    x.append(image)
+    label = data[data.Image == path]['Id_int'].values[0]
 
-    cod = train[train.Image == path]['Id_int'].values[0]
-    y.append(cod)
+    return image, label
 
 
-print(np.shape(x))
-print(np.shape(y))
-y
-#x = x.astype('float32')
+def loop_over_data(data):
+    X = []
+    y = []
+
+    for index, path in tqdm(enumerate(data.Image)):
+        if index>10:
+            break
+        image, label = get_image_from_path(path, training_dir, data)
+
+        X.append(image)
+        y.append(label)
+
+    return np.array(X), y
+
+
+X_train, y_train = loop_over_data(train)
+X_test, y_test = loop_over_data(test)
+
+
+print('X_train: ', np.shape(X_train))
+print('y_train: ', np.shape(y_train))
+
+print('X_test: ', np.shape(X_test))
+print('y_test: ', np.shape(y_test))
 
 # One hot encoding
-train_Y_one_hot = to_categorical(y)
+train_Y_one_hot = to_categorical(y_train, num_classes=NUM_OF_CLASSES)
+test_Y_one_hot = to_categorical(y_test, num_classes=NUM_OF_CLASSES)
 print('After conversion to one-hot:', len(train_Y_one_hot[0]))
 print('shape train_Y_one_hot: ', np.shape(train_Y_one_hot))
 
@@ -75,15 +88,16 @@ k_size = (4, 4)
 drop_probability = 0.5
 hidden_size = 256
 batch_size = 64
-input_shape = (128, 128, 3)
-pool_size = (2,2)
+input_shape = (128, 128, 1)
+pool_size = (2, 2)
 learning_rate = 0.07
-num_of_epochs = 1
-num_of_classes = 4251 # len(np.unique(y))# 4251
-print('num_of_classes: ', num_of_classes)
+num_of_epochs = 3
+
+print('num_of_classes: ', NUM_OF_CLASSES)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=input_shape,padding='same'))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='linear',
+          input_shape=input_shape, padding='same'))
 model.add(LeakyReLU(alpha=0.1))
 model.add(MaxPooling2D((2, 2),padding='same'))
 model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
@@ -95,7 +109,7 @@ model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
 model.add(Flatten())
 model.add(Dense(128, activation='linear'))
 model.add(LeakyReLU(alpha=0.1))
-model.add(Dense(num_of_classes, activation='softmax'))
+model.add(Dense(NUM_OF_CLASSES, activation='softmax'))
 
 
 model.compile(loss=categorical_crossentropy,
@@ -103,4 +117,11 @@ model.compile(loss=categorical_crossentropy,
               metrics=['accuracy'])
 model.summary()
 
-model.fit(np.array(x), train_Y_one_hot, batch_size=batch_size, epochs=num_of_epochs, verbose=1)
+model.fit(X_train, train_Y_one_hot, batch_size=batch_size,
+          epochs=num_of_epochs, verbose=1)
+
+model.save('whale.h5py')
+test_eval = model.evaluate(X_test, test_Y_one_hot, verbose=1)
+
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
